b729b4393d98249faef4744c93f526f092195d25
Natalie Taktachev <ntaktach@gmail.com>
2021-11-13 15:56:25 -0500

    add function to validate input

--- index.js
+++ index.js
@@ -5,6 +5,18 @@
 //add event listener to button and capture input on click 
 document.getElementById("button").addEventListener("click", function () {
     let data = document.getElementById('search_item').value
-    console.log(data);
+    validate(data);
 });
 
+function validate(data) {
+    const lettersOnly = /^[A-Za-z]+$/;
+    if (data.match(lettersOnly))
+        // search();
+        console.log("valid entry")
+    else
+        alert ("invalid input. please use a-z characters only")
+}
+
+function search(){
+    
+}
